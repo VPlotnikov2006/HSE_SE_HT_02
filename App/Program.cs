588bdbc5d4423eef108bff4f729a7c7151497d00
Plotnikov V.D. <pltnikovqwerty@gmail.com>
2025-11-05 18:09:22 +0300

    Move extensions to another file

--- App/Program.cs
+++ App/Program.cs
@@ -13,44 +13,6 @@ using App.UserActions;
 using App.UserActions.Create;
 using Microsoft.Extensions.DependencyInjection;
 
-public static class ServiceProviderKeyedExtensions
-{
-    public static Dictionary<object, T> GetKeyedServicesDictionary<T>(
-        this IServiceProvider provider,
-        IEnumerable<object> keys)
-        where T : class
-    {
-        var dict = new Dictionary<object, T>();
-
-        foreach (var key in keys)
-        {
-            var service = provider.GetKeyedService<T>(key);
-            if (service is not null)
-                dict[key] = service;
-        }
-
-        return dict;
-    }
-
-    public static Dictionary<KeyType, T> GetKeyedServicesDictionary<T, KeyType>(
-        this IServiceProvider provider,
-        IEnumerable<KeyType> keys)
-        where T : class
-        where KeyType : notnull
-    {
-        var dict = new Dictionary<KeyType, T>();
-
-        foreach (var key in keys)
-        {
-            var service = provider.GetKeyedService<T>(key);
-            if (service is not null)
-                dict[key] = service;
-        }
-
-        return dict;
-    }
-}
-
 internal static class Program
 {
     public static void Main()
---
+++ App/ServiceProviderExtension.cs
@@ -0,0 +1,41 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace App;
+
+public static class ServiceProviderKeyedExtensions
+{
+    public static Dictionary<object, T> GetKeyedServicesDictionary<T>(
+        this IServiceProvider provider,
+        IEnumerable<object> keys)
+        where T : class
+    {
+        var dict = new Dictionary<object, T>();
+
+        foreach (var key in keys)
+        {
+            var service = provider.GetKeyedService<T>(key);
+            if (service is not null)
+                dict[key] = service;
+        }
+
+        return dict;
+    }
+
+    public static Dictionary<KeyType, T> GetKeyedServicesDictionary<T, KeyType>(
+        this IServiceProvider provider,
+        IEnumerable<KeyType> keys)
+        where T : class
+        where KeyType : notnull
+    {
+        var dict = new Dictionary<KeyType, T>();
+
+        foreach (var key in keys)
+        {
+            var service = provider.GetKeyedService<T>(key);
+            if (service is not null)
+                dict[key] = service;
+        }
+
+        return dict;
+    }
+}
